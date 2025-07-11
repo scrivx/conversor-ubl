08ea01a6aeb8c158ae373fd52b219f55889f34f9
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-07-11 15:23:42 -0500

    Add: signer UBL format

--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module github.com/scrivx/conversor-ubl
 go 1.24.4
 
 require (
-	github.com/beevik/etree v1.5.1
+	github.com/amdonov/xmlsig v0.1.0
 	github.com/gin-gonic/gin v1.10.1
-	github.com/russellhaering/goxmldsig v1.5.0
 	software.sslmate.com/src/go-pkcs12 v0.5.0
 )
 
@@ -19,7 +18,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
-	github.com/jonboulle/clockwork v0.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -36,5 +34,6 @@ require (
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/pkg/signature/signer.go
+++ internal/pkg/signature/signer.go
@@ -1,18 +1,18 @@
 package signature
 
 import (
-	"crypto"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
 	"os"
 
-	dsig "github.com/russellhaering/goxmldsig"
+	"github.com/amdonov/xmlsig"
 	"software.sslmate.com/src/go-pkcs12"
 )
 
-func LoadKeyPairFromPFX(pfxPath string, password string) (*x509.Certificate, crypto.PrivateKey, error) {
+func LoadKeyPairFromPFX(pfxPath, password string) (*x509.Certificate, *rsa.PrivateKey, error) {
 	pfxData, err := os.ReadFile(pfxPath)
 	if err != nil {
 		return nil, nil, err
@@ -23,34 +23,96 @@ func LoadKeyPairFromPFX(pfxPath string, password string) (*x509.Certificate, cry
 		return nil, nil, err
 	}
 
-	return certificate, privateKey, nil
+	rsaKey, ok := privateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, nil, errors.New("clave privada no es RSA")
+	}
+
+	return certificate, rsaKey, nil
 }
 
-func SignXML(xmlInput string, cert *x509.Certificate, key crypto.PrivateKey) (string, error) {
-	ctx := dsig.NewDefaultSigningContext(dsig.NewSigner(privateRSAKey(key)))
-	ctx.Hash = crypto.SHA1
+func SignXML(xmlInput string, cert *x509.Certificate, key *rsa.PrivateKey) (string, error) {
+	if cert == nil || key == nil {
+		return "", errors.New("certificado o clave privada no pueden ser nil")
+	}
 
-	ctx.SetSignatureMethod(dsig.RSASHA1SignatureMethod)
-	ctx.Canonicalizer = dsig.MakeC14N10ExclusiveCanonicalizerWithPrefixList("")
+	// Convertir certificado y clave a formato tls.Certificate
+	tlsCert, err := createTLSCertificate(cert, key)
+	if err != nil {
+		return "", err
+	}
 
+	// Crear el firmador con xmlsig
+	signer, err := xmlsig.NewSigner(tlsCert)
+	if err != nil {
+		return "", err
+	}
+
+	// Firmar el XML directamente
+	signedBytes, err := signer.Sign([]byte(xmlInput))
+	if err != nil {
+		return "", err
+	}
+
+	return string(signedBytes), nil
+}
+
+// Función auxiliar para convertir x509.Certificate y rsa.PrivateKey a tls.Certificate
+func createTLSCertificate(cert *x509.Certificate, key *rsa.PrivateKey) (tls.Certificate, error) {
+	// Convertir certificado a PEM
 	certPEM := pem.EncodeToMemory(&pem.Block{
 		Type:  "CERTIFICATE",
 		Bytes: cert.Raw,
 	})
 
-	ctx.SetCertificateChain([][]byte{cert.Raw})
+	// Convertir clave privada a PEM
+	keyBytes, err := x509.MarshalPKCS8PrivateKey(key)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
 
-	signedXML, err := ctx.SignEnveloped([]byte(xmlInput))
+	keyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: keyBytes,
+	})
+
+	// Crear tls.Certificate desde PEM
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	return tlsCert, nil
+}
+
+// Función alternativa con opciones personalizadas
+func SignXMLWithOptions(xmlInput string, cert *x509.Certificate, key *rsa.PrivateKey) (string, error) {
+	if cert == nil || key == nil {
+		return "", errors.New("certificado o clave privada no pueden ser nil")
+	}
+
+	// Convertir a tls.Certificate
+	tlsCert, err := createTLSCertificate(cert, key)
 	if err != nil {
 		return "", err
 	}
 
-	return string(signedXML), nil
-}
-
-func privateRSAKey(key crypto.PrivateKey) *rsa.PrivateKey {
-	if rsaKey, ok := key.(*rsa.PrivateKey); ok {
-		return rsaKey
+	// Crear firmador con opciones personalizadas
+	options := xmlsig.SignerOptions{
+		SignatureAlgorithm: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
+		DigestAlgorithm:    "http://www.w3.org/2001/04/xmlenc#sha256",
 	}
-	panic(errors.New("clave privada no es RSA"))
+
+	signer, err := xmlsig.NewSignerWithOptions(tlsCert, options)
+	if err != nil {
+		return "", err
+	}
+
+	// Firmar el XML
+	signedBytes, err := signer.Sign([]byte(xmlInput))
+	if err != nil {
+		return "", err
+	}
+
+	return string(signedBytes), nil
 }
