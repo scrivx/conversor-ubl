87d5fb36100a3ef10039577658bf7956d0367644
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-07-14 18:58:39 -0500

    update project LOAD

--- tests/test_unpload.sh
+++ tests/test_unpload.sh
@@ -1,3 +1,3 @@
-curl -X POST http://localhost:8080/upload -H "Content-Type: application/json" -d '{
-    "invoice_id": "F001-123"
+curl -X POST http://localhost:8080/load -H "Content-Type: application/json" -d '{
+    "invoice_id": "20123456789-F-001-204"
 }'
